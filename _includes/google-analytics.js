a5714f35f1c3c451196caad8c11263c65caa38ac
williamchiu <williamchiu16@gmail.com>
2017-02-06 15:25:16 -0500

    try fooling google pagespeed

--- _includes/google-analytics.js
+++ _includes/google-analytics.js
@@ -1,8 +1,13 @@
-<!-- Google Analytics -->
 <script>
-window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-ga('create', 'UA-61513926-2', 'auto');
-ga('send', 'pageview');
+eval(google_analytics);
+
+function google_analytics() {
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-61513926-2', 'auto');
+	ga('send', 'pageview');
+}
 </script>
-<script async src='https://www.google-analytics.com/analytics.js'></script>
-<!-- End Google Analytics -->
