632d540f3532efe2f761563a9900b2d8c4554e99
williamchiu <williamchiu16@gmail.com>
2017-03-05 04:35:13 -0500

    travis

--- _includes/hide-assides.js
+++
@@ -1,9 +0,0 @@
-{% comment %}
-Admittedly needs to be rethought. By reducing asides of the site, the thought is to emphasize sharing content over sharing the site itself.
-{% endcomment %}
-<script>
-if (window.location.search.indexOf('hide-asides') != -1) {
-	console.log("Hiding assides...");
-	document.styleSheets[0].insertRule('.aside { display: none; }', 0);
-}
-</script>
